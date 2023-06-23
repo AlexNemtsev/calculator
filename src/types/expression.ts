f3ff844dfa40fcbe9b4b3364bf539049b43f3450
Alexander Nemtsev <mrcrazy696@gmail.com>
2023-06-23 07:52:17 +0300

    fix: add Operation predicate

--- src/types/expression.ts
+++ src/types/expression.ts
@@ -1,5 +1,13 @@
 type Paranthesis = '(' | ')';
 
-export type Operation = '+' | '-' | '÷' | '×';
+const operations = ['+', '-', '÷', '×'] as const;
+
+export type Operation = (typeof operations)[number];
 
 export type Expression = number | Paranthesis | Operation;
+
+export const isOperation = (value: string): value is Operation => {
+  const values: string[] = [];
+  operations.forEach((val) => values.push(val));
+  return values.includes(value);
+};
