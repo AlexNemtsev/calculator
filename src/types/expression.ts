773bb61e23033b3f6b3c61e096ff9d581a54d564
Alexander Nemtsev <55802702+AlexNemtsev@users.noreply.github.com>
2023-06-27 11:48:46 +0000

    feat: add isParanthesis function

--- src/types/expression.ts
+++ src/types/expression.ts
@@ -1,13 +1,18 @@
-type Paranthesis = '(' | ')';
-
+const paranthesis = ['(', ')'] as const;
 const operations = ['+', '-', '÷', '×'] as const;
 
+export type Paranthesis = (typeof paranthesis)[number];
 export type Operation = (typeof operations)[number];
-
 export type Expression = number | Paranthesis | Operation;
 
 export const isOperation = (value: string): value is Operation => {
   const values: string[] = [];
   operations.forEach((val) => values.push(val));
+  return values.includes(value);
+};
+
+export const isParanthesis = (value: string): value is Paranthesis => {
+  const values: string[] = [];
+  paranthesis.forEach((val) => values.push(val));
   return values.includes(value);
 };
